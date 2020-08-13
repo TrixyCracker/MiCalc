82475520d0b85ad3195370655d0ca51787783b3d
xGabbro <sergabbro@gmail.com>
2020-08-13 02:25:35 +0200

    Fixed a few bug and rewrited code

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-
 //Libraries
 #include <stdio.h>
 
@@ -13,23 +12,36 @@ void uid_to_u8(u8 _Dest[8], char _UID[9]);
 u8 char_to_u8(char _c);
 
 //Functions
-int main() {
+int main(void) {
 
-	//Declaration of key container
 	u8 KeyA[5][6] = {
-		{ 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5 }
+		[0] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5 }
 	};
 	u8 KeyB[5][6] = {
-		{ 0xB4, 0xC1, 0x32, 0x43, 0x9E, 0xEF }
+		[0] = { 0xB4, 0xC1, 0x32, 0x43, 0x9E, 0xEF }
 	};
 
-	//Code
 	char UID_string[9];
 	printf("Enter the UID of MiZip: 0x");
 	scanf("%s", UID_string);
 
-	u8 UID[8];
-	uid_to_u8(UID, UID_string);
+	//Check if is valid UID
+	for (int i = 0; i < 8; i++) 
+	{	
+		if ( char_to_u8(UID_string[i]) == 0xFF ) 
+		{
+			printf("[!] Please enter a valid UID. \n");
+			return 1;
+		}
+	}
+	//---
+
+	//UID string to uint8 array
+	u8 UID[4];
+	for (int i = 0; i < 4; i++) {
+		UID[i] = (char_to_u8(UID_string[i * 2]) << 4) + char_to_u8(UID_string[(i * 2) + 1]);
+	}
+	//---
 
 	printf("UID: 0x");
 	for (int i = 0; i < 4; i++) printf("%02X", UID[i]);
@@ -51,10 +63,9 @@ int main() {
 		for (int i2 = 0; i2 < 6; i2++) printf("%02X", KeyB[i][i2]);
 		printf("\n");
 
-
 	}
-
-	getchar();
+	
+	fflush(stdin);
 	getchar();
 
 	return 0;
@@ -66,11 +77,10 @@ void calculate_keyA(u8 _Dest[6], u8 _UID[4], int _Block)
 	const int byte_rules[6] = { 0, 1, 2, 3, 0, 1 };
 
 	const u8 xor_table_keyA[5][6] = {
-		{},
-		{ 0x09, 0x12, 0x5A, 0x25, 0x89, 0xE5 },
-		{ 0xAB, 0x75, 0xC9, 0x37, 0x92, 0x2F },
-		{ 0xE2, 0x72, 0x41, 0xAF, 0x2C, 0x09 },
-		{ 0x31, 0x7A, 0xB7, 0x2F, 0x44, 0x90 }
+		[1] = { 0x09, 0x12, 0x5A, 0x25, 0x89, 0xE5 },
+		[2] = { 0xAB, 0x75, 0xC9, 0x37, 0x92, 0x2F },
+		[3] = { 0xE2, 0x72, 0x41, 0xAF, 0x2C, 0x09 },
+		[4] = { 0x31, 0x7A, 0xB7, 0x2F, 0x44, 0x90 }
 	};
 
 	//Code
@@ -89,11 +99,10 @@ void calculate_keyB(u8 _Dest[6], u8 _UID[4], int _Block)
 	const int byte_rules[6] = { 2, 3, 0, 1, 2, 3 };
 
 	const u8 xor_table_keyB[5][6] = {
-		{},
-		{ 0xF1, 0x2C, 0x84, 0x53, 0xD8, 0x21 },
-		{ 0x73, 0xE7, 0x99, 0xFE, 0x32, 0x41 },
-		{ 0xAA, 0x4D, 0x13, 0x76, 0x56, 0xAE },
-		{ 0xB0, 0x13, 0x27, 0x27, 0x2D, 0xFD }
+		[1] = { 0xF1, 0x2C, 0x84, 0x53, 0xD8, 0x21 },
+		[2] = { 0x73, 0xE7, 0x99, 0xFE, 0x32, 0x41 },
+		[3] = { 0xAA, 0x4D, 0x13, 0x76, 0x56, 0xAE },
+		[4] = { 0xB0, 0x13, 0x27, 0x27, 0x2D, 0xFD }
 	};
 
 	//Code
@@ -102,16 +111,6 @@ void calculate_keyB(u8 _Dest[6], u8 _UID[4], int _Block)
 	for (int i = 0; i < 6; i++) 
 	{
 		_Dest[i] = _UID[byte_rules[i]] ^ xor_table_keyB[_Block][i];
-	}
-
-}
-
-void uid_to_u8(u8 _Dest[8], char _UID[9])
-{
-
-	for (int i = 0; i < 4; i++) 
-	{
-		_Dest[i] = char_to_u8(_UID[i * 2]) * 16 + char_to_u8(_UID[(i * 2) + 1]);
 	}
 
 }
@@ -129,11 +128,12 @@ u8 char_to_u8(char _c)
 	else if (_c == '7') return 0x7;
 	else if (_c == '8') return 0x8;
 	else if (_c == '9') return 0x9;
-	else if (_c == 'A') return 0xA;
-	else if (_c == 'B') return 0xB;
-	else if (_c == 'C') return 0xC;
-	else if (_c == 'D') return 0xD;
-	else if (_c == 'E') return 0xE;
-	else if (_c == 'F') return 0xF;
+	else if (_c == 'A' || _c == 'a') return 0xA;
+	else if (_c == 'B' || _c == 'b') return 0xB;
+	else if (_c == 'C' || _c == 'c') return 0xC;
+	else if (_c == 'D' || _c == 'd') return 0xD;
+	else if (_c == 'E' || _c == 'e') return 0xE;
+	else if (_c == 'F' || _c == 'f') return 0xF;
+	else return 0xFF;
 
 }
